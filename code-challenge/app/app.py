2a295be6390829476ac2e98cb9bac20c6ad06fa4
myrinja <myrinjau98@gmail.com>
2023-10-25 18:12:25 +0300

    migration and app.db created

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -6,12 +6,12 @@ from models import db, Hero, Power, HeroPower
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
-
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+#db.init_app(app)
 
 
 @app.route('/')
